35e5f0abc684bccb5a7d39b7ed38d0869f3b1bba
Mukul Parmar <mukul.parmar@yahoo.com>
2020-05-31 13:24:19 +0530

    Initial commit

---
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,13 @@
+package com.udacity.sandwichclub.utils;
+
+import android.content.Context;
+
+import com.udacity.sandwichclub.model.Sandwich;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json, Context applicationContext) {
+        return null;
+    }
+
+}
